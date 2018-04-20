5d5dade09d70f6746af160f42d953c8581d72795
Nick Van den Broeck <nick.van.den.broeck666@gmail.com>
2018-04-20 18:19:15 +0200

    Added a function to calculate the accuracy of a network
    Also written a Double wrapper to store accuracies.

---
+++ src/Grenade/Utils/Accuracy.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Grenade.Utils.Accuracy
+    ( Accuracy
+    , accuracyM
+    , showAccuracy
+    ) where
+
+import Data.Validity
+
+import Data.Aeson (ToJSON, FromJSON)
+
+import GHC.Generics
+
+import Control.Monad.Catch
+
+newtype Accuracy = Accuracy Double deriving (Show, Eq, Generic)
+
+data AccuracyOutOfBounds = AccuracyOutOfBounds deriving (Show, Eq, Generic)
+
+instance Exception AccuracyOutOfBounds where
+    displayException = const "The accuracy is not in [0,1]."
+
+accuracyM :: MonadThrow m => Double -> m Accuracy
+accuracyM x = case prettyValidation $ Accuracy x of
+    Right a -> pure a
+    Left _ -> throwM AccuracyOutOfBounds
+
+instance ToJSON Accuracy
+
+instance FromJSON Accuracy
+
+instance Validity Accuracy where
+    validate (Accuracy x) = 0 <= x && x <= 1 <?@> "The accuracy is in [0,1]"
+
+showAccuracy :: String -> Accuracy -> String
+showAccuracy name (Accuracy x) = "The " ++ name ++ " accuracy is "++  show (nOfPrecent * x) ++ "%."
+
+nOfPrecent :: Double
+nOfPrecent = 100
